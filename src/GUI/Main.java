301d0f45a8aacdebd893ed29aff38787c812b7fc
Ángel Alcantar <rayoalcantar@gmail.com>
2023-12-05 22:13:50 -0600

    poniendo todo otra vez

---
+++ src/GUI/Main.java
@@ -0,0 +1,11 @@
+package GUI;
+
+/**
+ *
+ * @author Pedro Quiroz
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Metodos/Operaciones.java
@@ -0,0 +1,13 @@
+package Metodos;
+
+/**
+ *
+ * @author Pedro Quiroz
+ */
+public class Operaciones {
+    //Comentario Prueba
+    //PRUEBA LUIS
+    //Comentario Prueba Gael, Holi uwu
+    // aqui estuvo yair 
+    //Holaaaa Jacque
+}
